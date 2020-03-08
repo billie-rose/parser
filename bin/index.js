0f605dab8a5e5f7bcd05d6f303f2efc7064ef16d
Billie Rose <billie.rose@zywave.com>
2020-03-07 22:23:42 -0600

    zz

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
-var ci = require('../lib/src/ci');
+const parse = require('../lib/src/cli/parse');
+const help = require('../lib/src/cli/help');
+const config = require('../lib/src/config');
+
+ci = (commands = []) => {
+    switch (commands.length) {
+        case 0:
+            parse();
+            break;
+        case 1:
+            commands[0] != 'parse' ? help() : parse();
+            break;
+        default:
+        //TODO Eventual handling of other options as specified in commands.js
+    }
+};
 
 ci(process.argv.slice(2));
